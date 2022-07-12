e7b525a90c3ceb315d0face4c40aea3ff4655b43
Israel Bernabe <israel.bernabe@gmail.com>
2022-07-12 05:45:30 -0400

    Adding book integration test

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+
+
+const {generateManyBook} = require('../src/fakes/book.fake');
+
+const mockGetAll = jest.fn();
+jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {
+  },
+})));
+
+const createApp = require('../src/app');
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test for [GET] /api/v1/books', () => {
+    test('should return a list of books', () => {
+      //Given
+      const fakeBooks = generateManyBook(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      //
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({body}) => {
+          console.log('body', body);
+          expect(body.length).toEqual(3);
+        });
+    });
+  });
+});
